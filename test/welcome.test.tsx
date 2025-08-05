9099d7f89ab7dd6cbb5e4a022c2f7686f71e9efb
lolieatapple <lolieatapple@gmail.com>
2025-08-05 12:10:41 +0800

    first add

---
+++ test/welcome.test.tsx
@@ -0,0 +1,32 @@
+import renderer from 'react-test-renderer';
+import React from 'react';
+import { expect, test } from 'vitest';
+import App from '../src/App';
+import { DAppKitProvider } from '@vechain/dapp-kit-react';
+import { WalletConnectOptions } from '@vechain/dapp-kit';
+
+test('Welcome', async () => {
+    const walletConnectOptions: WalletConnectOptions = {
+        projectId: 'a0b855ceaf109dbc8426479a4c3d38d8',
+        metadata: {
+            name: 'Sample VeChain dApp',
+            description: 'A sample VeChain dApp',
+            url: window.location.origin,
+            icons: [``],
+        },
+    };
+    const component = renderer.create(
+        <React.StrictMode>
+            <DAppKitProvider
+                node={'https://testnet.vechain.org/'}
+                usePersistence
+                walletConnectOptions={walletConnectOptions}
+            >
+                <App />
+            </DAppKitProvider>
+        </React.StrictMode>,
+    );
+
+    const tree = component.toJSON();
+    expect(tree).toBeDefined();
+});
